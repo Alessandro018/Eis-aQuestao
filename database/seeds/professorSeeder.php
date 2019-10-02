a7c943b427dc5c8cac13fc603b13bd73b8c7ddec
robson-gomes <robson.gs2630@gmail.com>
2019-10-02 14:43:27 -0300

    GH-21 Alterando a coluna siepe da tabela professor no seeds

--- database/seeds/professorSeeder.php
+++ database/seeds/professorSeeder.php
@@ -13,25 +13,25 @@ class professorSeeder extends Seeder
     {
         $professor = [
             0 => [
-                'siap' => 'ABC001',
+                'siape' => 'ABC001',
                 'campus' => 'Igarassu',
                 'nome' => 'Ranieri Valença',
                 'senha' => 'ifpe'
             ],
             1 => [
-                'siap' => 'DEF002',
+                'siape' => 'DEF002',
                 'campus' => 'Igarassu',
                 'nome' => 'Liliane Alves',
                 'senha' => 'ifpe'
             ],
             2 => [
-                'siap' => 'GHI003',
+                'siape' => 'GHI003',
                 'campus' => 'Igarassu',
                 'nome' => 'Lara Régia',
                 'senha' => 'ifpe'
             ],
             3 => [
-                'siap' => 'JLM004',
+                'siape' => 'JLM004',
                 'campus' => 'Igarassu',
                 'nome' => 'Milton Junior',
                 'senha' => 'ifpe'
