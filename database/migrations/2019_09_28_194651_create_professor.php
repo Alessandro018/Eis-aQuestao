f66d4bef3c397fbf4084e12224498af4d1a04ab5
Alessandro018 <alessandrosilva325@gmail.com>
2019-10-01 21:39:53 -0300

    GH-18 Alteração na coluna siape da tabela professor

--- database/migrations/2019_09_28_194651_create_professor.php
+++ database/migrations/2019_09_28_194651_create_professor.php
@@ -15,7 +15,7 @@ class CreateProfessor extends Migration
     {
         Schema::create('professor', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('siap');
+            $table->string('siape')->unique();
             $table->string('campus');
             $table->string('nome');
             $table->string('senha');
