030ada23daed2d7bc281a57993d3cc2af201b4c1
Alessandro018 <alessandrosilva325@gmail.com>
2019-10-07 18:56:16 -0300

    GH-31 Adicionando nome da tabela no model

--- app/Questao.php
+++ app/Questao.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
    
 class Questao extends Model
 {
+    protected $table = "questao";
+    
     protected $fillable = [
         'pergunta','tipo', 'nivel', 'professor_id', 'disciplina_id'
     ];
