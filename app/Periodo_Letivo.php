43f5064bc70816f445e5604cb2d3cba6874db3d5
siilvatamires <tamiresmaria561@gmail.com>
2019-10-07 18:56:14 -0300

    GH-36 Adicionando atributos na model periodo_letivo

--- app/Periodo_Letivo.php
+++ app/Periodo_Letivo.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Periodo_Letivo extends Model
 {
-    //
+    protected $table="periodo_letivo";
+protected $fillable = [
+        'ano', 'semestre'
+    ];
 }
