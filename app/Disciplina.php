9c44347181ef8b63a26aa1b13aac699122704096
siilvatamires <tamiresmaria561@gmail.com>
2019-10-06 22:12:36 -0300

    GH-36 Adicionando atributos à model disciplina

--- app/Disciplina.php
+++ app/Disciplina.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Disciplina extends Model
 {
-    //
+    protected $table="disciplina";
+protected $fillable = [
+        'nome', 'curso_id'
+    ];
 }
