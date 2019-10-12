c774b84abab5fde4f3e74755b48ae12af6de1ec8
robson-gomes <robson.gs2630@gmail.com>
2019-10-12 00:03:29 -0300

    GH-46 Adicão de atributos

--- app/Professor_Disciplina.php
+++ app/Professor_Disciplina.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor_Disciplina extends Model
 {
-    //
+     protected $table="professores_disciplinas";
+    protected $fillable = [
+        'disciplina_id','professor_id', 'periodo_letivo_id'
+    ];
 }
