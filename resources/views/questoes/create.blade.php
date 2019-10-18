c19cf15cb3c24f76fa789f9cd356cf840d6562cf
gabrielpessoa <gabrielpessoanascimento@gmail.com>
2019-10-18 20:48:09 -0300

    GH-41 view de inserir alternativa

--- resources/views/questoes/create.blade.php
+++ resources/views/questoes/create.blade.php
@@ -32,15 +32,16 @@
             <small id="emailHelp" class="form-text text-muted">Disciplina em que a questão pertence</small>
         </div>
         <div class="form-group">
-            <label>Tipo</label><br>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="tipo" id="inlineRadio1" value="aberta">
-                <label class="form-check-label" for="inlineRadio1">Aberta</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="tipo" id="inlineRadio2" value="fechada">
-                <label class="form-check-label" for="inlineRadio2">Fechada</label>
-            </div>
+            <input type="hidden" name="tipo" value="fechada"> 
+            <?php
+                $letras=['A','B','C','D','E'];
+            ?>
+            @foreach ($letras as $letra)
+                <input type="radio" name="correta" value="correta{{$letra}}">
+                <label>{{$letra}})</label>
+                <textarea class="form-control" maxlength="255" name="alternativa{{$letra}}"
+            placeholder="Máximo de 255 caractéres" required></textarea>
+            @endforeach
         </div>
         <input type="hidden" name="professor_id" value="1">
         <button type="submit" class="btn btn-primary">Submit</button>
