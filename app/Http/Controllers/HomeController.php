52464f41c52066df924f3d4279d9a3ca419e0ce2
Alessandro018 <alessandrosilva325@gmail.com>
2019-10-30 00:32:15 -0300

    GH-29 Back-end da página de períodos Página onde lista todos os períodos onde o professor logado tem disciplinas cadastradas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Professor;
+use App\Professor_Disciplina;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if(auth()->check())
+        {
+            $prof_periodo = Professor_Disciplina::join('periodos_letivos', 'periodos_letivos.id', '=', 'professores_disciplinas.periodo_letivo_id' )
+            ->select('periodos_letivos.*')
+            ->where('professores_disciplinas.professor_id', auth()->user()->id)->get();
+            return view('home', ['professor_periodo' => $prof_periodo]);
+        }
+        return redirect()->route('login');
     }
 }
