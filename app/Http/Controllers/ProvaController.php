26773e0c072c13d4198c0d04c514afa0a5a73fa0
Alessandro018 <alessandrosilva325@gmail.com>
2019-11-01 22:11:06 -0300

    GH-3 Início da geração de provas

--- app/Http/Controllers/ProvaController.php
+++ app/Http/Controllers/ProvaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Professor;
+use App\Prova;
+use App\Questao;
 use Illuminate\Support\Facades\DB;
 
 class ProvaController extends Controller
@@ -17,8 +19,31 @@ class ProvaController extends Controller
         return view('provas.index', ['professor_disciplina' => $professor_disciplina]);
     }
 
-    public function store()
+    public function teste(Request $request)
     {
-        return "c";
+        $request->validate([
+            'disciplina' => 'required',
+            'professor_id' => 'required',
+            'cabecalho' => 'required',
+            'nivel1' => 'required',
+            'nivel2' => 'required',
+            'nivel3' => 'required',
+        ]);
+        if($request->nivel1 >0)
+        {
+            $questao1 = Questao::with('alternativas')->where('questoes.disciplina_id', $request->disciplina)
+            ->where('questoes.nivel',1)->limit($request->nivel1)->get();
+        }
+        if($request->nivel2 >0)
+        {
+            $questao2 = Questao::with('alternativas')->where('questoes.disciplina_id', $request->disciplina)
+            ->where('questoes.nivel',2)->inRandomOrder()->limit($request->nivel2)->get();
+        }
+        if($request->nivel3 >0)
+        {
+            $questao3 = Questao::with('alternativas')->where('questoes.disciplina_id', $request->disciplina)
+            ->where('questoes.nivel',3)->inRandomOrder()->limit($request->nivel3)->get();
+        }
+        
     }
 }
