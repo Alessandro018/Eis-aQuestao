bc8c5f429d42b6a0a05a39f04ba043b6d417082b
siilvatamires <tamiresmaria561@gmail.com>
2019-11-15 22:38:46 -0300

    GH-50 Alterando alguns campos para o português

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-3">
-                    <h5>Get started</h5>
+                    <h5>Iniciar</h5>
                     <ul>
                         <li><a href="#">Home</a></li>
                         <li><a href="#">Sign up</a></li>
@@ -63,7 +63,7 @@
                     </ul>
                 </div>
                 <div class="col-sm-3">
-                    <h5>About us</h5>
+                    <h5>Sobre nós</h5>
                     <ul>
                         <li><a href="#">Company Information</a></li>
                         <li><a href="#">Contact us</a></li>
@@ -71,7 +71,7 @@
                     </ul>
                 </div>
                 <div class="col-sm-3">
-                    <h5>Support</h5>
+                    <h5>Suporte</h5>
                     <ul>
                         <li><a href="#">FAQ</a></li>
                         <li><a href="#">Help desk</a></li>
@@ -79,7 +79,7 @@
                     </ul>
                 </div>
                 <div class="col-sm-3 info">
-                    <h5>Information</h5>
+                    <h5>Informações</h5>
                     <p> Lorem ipsum dolor amet, consectetur adipiscing elit. Etiam consectetur aliquet aliquet. Interdum et malesuada fames ac ante ipsum primis in faucibus. </p>
                 </div>
             </div>
