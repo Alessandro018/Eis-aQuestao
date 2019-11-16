2bc3cd98bcde8adddb06bf667f50ccfbd132ee5c
filipeandrev <filipe.andrev7@gmail.com>
2019-11-15 23:24:35 -0300

    GH-77 Retirando botao com modal da view index

--- resources/views/questoes/index.blade.php
+++ resources/views/questoes/index.blade.php
@@ -8,8 +8,6 @@
         </div>
     @endif
 
-	<button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#exampleModalLong">+ Criar Questão</button>
-
 	<table class="table table-sm mt-4">
 		<tr>
 			<th>Pergunta</th>
@@ -18,26 +16,6 @@
 			<th>Disciplina</th>
 			<th>Ações</th>
 		</tr>
-
-		<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">&times;</span>
-						</button>
-					</div>
-					<div class="modal-body">
-						...
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
-					</div>
-				</div>
-			</div>
-		</div>
 
 		@foreach ($questoes as $questao)
 		<tr>
