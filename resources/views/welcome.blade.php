4fee2ffbb768b7485cdb22948a5043d3d350327f
robson-gomes <robson.gs2630@gmail.com>
2019-11-18 01:25:16 -0300

    GH-70 Modificando carrossel da página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,66 +1,48 @@
 @extends('layouts.app')
-
+        
 @section('content')
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <div class="carousel-caption">
-            <h3>hevsflebkjfbwajsb</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-6 col-sm-4">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+              <img src="{{asset('img/ifpe1.png')}}" class="d-block w-100" alt="...">
         </div>
-        <img src="https://wallpapersite.com/images/pages/pic_w/14372.png" alt="Los Angeles">
-      </div>
-
-      <div class="item">
-        <div class="carousel-caption">
-            <h3>hevsflebkjfbwajsb</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="carousel-item">
+              <img src="{{asset('img/ifpe2.png')}}" class="d-block w-100" alt="...">
         </div>
-        <img src="https://wallpapersite.com/images/pages/pic_w/14372.png" alt="Chicago">
-      </div>
-    
-      <div class="item">
-        <div class="carousel-caption">
-            <h3>hevsflebkjfbwajsb</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div class="carousel-item">
+              <img src="{{asset('img/ifpe3.png')}}" class="d-block w-100" alt="...">
         </div>
-        <img src="https://wallpapersite.com/images/pages/pic_w/14372.png" alt="New york">
       </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
+    </div>
+    <div class="col-6 col-sm-8">
+      <h3><u>Crie suas avaliações. É fácil e rápido! Gere provas diferentes para cada aluno.
+      </u></h3>
+    <p>Elabore suas avaliações no nosso site e as corrija com seu smartphone. Esta é a idéia do Eis a Questão.</p>
+    <p>Diminua o tempo para elaboração de avaliações, reutilizando suas questões cadastradas, ou selecionando questões do banco de questões.</p> Imprima tipos diferentes de provas embaralhando questões e alternativas.
+    </div>
+    <div class="col">
+      <table class="table">
+      <thead>
+        <tr>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+  </div>
   </div>
 </div>
 @endsection
+
