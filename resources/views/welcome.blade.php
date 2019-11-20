59a17d751b29264a5e46406a6e94832eef911e27
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-11-20 02:43:00 -0300

    GH-70 alterando front-end da pagina inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,49 @@
 @extends('layouts.app')
         
 @section('content')
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-6 col-sm-4">
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-      <div class="carousel-inner">
+
+<div id="carouselExampleControls" class="carousel slide p-5" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+        <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner img-car">
         <div class="carousel-item active">
-              <img src="{{asset('img/ifpe1.png')}}" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h2 class="text-dark"></h2>
+            </div>
+            <img src="{{ asset('img/ifpe1.png') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-              <img src="{{asset('img/ifpe2.png')}}" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h2 class="text-dark">Diminua o tempo para elaboração de avaliações, reutilizando suas questões cadastradas, ou selecionando questões do banco de questões.</h2>
+            </div>
+            <img src="{{ asset('img/ifpe2.png') }}" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-              <img src="{{asset('img/ifpe3.png')}}" class="d-block w-100" alt="...">
+            <div class="carousel-caption">
+                <h2 class="text-dark">Imprima tipos diferentes de provas embaralhando questões e alternativas.</h2>
+            </div>
+            <img src="{{ asset('img/ifpe3.png') }}" class="d-block w-100" alt="...">
         </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    </a>
+    <a class="carousel-control-next"  href="#carouselExampleControls" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
-      </a>
-    </div>
-    </div>
-    <div class="col-6 col-sm-8">
-      <h3><u>Crie suas avaliações. É fácil e rápido! Gere provas diferentes para cada aluno.
-      </u></h3>
-    <p>Elabore suas avaliações no nosso site e as corrija com seu smartphone. Esta é a idéia do Eis a Questão.</p>
-    <p>Diminua o tempo para elaboração de avaliações, reutilizando suas questões cadastradas, ou selecionando questões do banco de questões.</p> Imprima tipos diferentes de provas embaralhando questões e alternativas.
-    </div>
+    </a>
+</div>
+
+<div class="container p-5">
+  <div class="row justify-content-start">
     <div class="col">
-      <table class="table">
-      <thead>
-        <tr>
-        </tr>
-      </thead>
-      <tbody>
-      </tbody>
-    </table>
-  </div>
+        <h1 class="text-center">Crie suas avaliações. É fácil e rápido! Gere provas diferentes para cada aluno.</h1>
+        <a href=""></a>
+    </div>
   </div>
 </div>
 @endsection
-
