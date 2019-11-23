a1eaa7e73a8aa72bc4611bfa41be80b83b49e7c3
siilvatamires <tamiresmaria561@gmail.com>
2019-11-23 14:31:32 -0300

    GH-85 Remoção de códigos desnecessários da home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>Periodo letivo</h2><br>
-<div class="container">
+<!-- <h2>Periodo letivo</h2><br>
+ --><div class="container">
     <div class="row">
         @foreach($professor_periodo as $key => $periodo)
-        <div class="col" style="margin-left: 65px">
+       <!--  <div class="col" style="margin-left: 65px">
             <p><a href="">{{$periodo->ano}}.{{$periodo->semestre}}</a></p>
-        </div>
+        </div> -->
         @if($key==4)
             <div class="w-100"></div>
         @endif
