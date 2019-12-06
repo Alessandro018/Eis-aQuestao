b84dcef3edcad09c7798c138140327cdbe184b43
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-12-05 23:34:16 -0300

    GH-85 Alterando layout da pagína

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,52 +2,54 @@
             
     @section('content')
 
-    <div id="carouselExampleControls" class="row carousel slide p-5 mt-5" data-ride="carousel">
-        <ul class="carousel-indicators">
-            <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleControls" data-slide-to="1"></li>
-            <li data-target="#carouselExampleControls" data-slide-to="2"></li>
-        </ul>
-        <div class="carousel-inner img-car">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row justify-content-md-center">
-                        <div class="col-md-auto">
-                           <img class="card-img-top img-width-1" src="{{ asset('img/ifpe3.png') }}" alt="Card image">
-                            <h4 class="mt-5 text-dark text-center">Questões com níveis de dificuldades.  
-                            </h4>
-                        </div>
-                    </div>
-                </div>
+<img class="d-block w-100" src="{{ asset('img/layout.png') }}" alt="Card image">
+
+<h1 class="display-1 text-center mt-5">Eis-aQuestão</h1>
+
+<div class="container mt-5">
+  <div class="row justify-content-md-center">
+    <div class="col col-lg-4 mt-4">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="color text-center">Coreção com QRcode.</h5>
             </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row justify-content-md-center">
-                        <div class="col-md-auto">
-                             <img class="card-img-top img-width-2" src="{{ asset('img/ifpe2.png') }}" alt="Card image">
-                             <h4 class="mt-5 text-dark text-center">Correção com QRcode.</h4>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row justify-content-md-center">
-                        <div class="col-md-auto">
-                            <img class="card-img-top img-width-3" src="{{ asset('img/ifpe1.png') }}"alt="Card image">
-                             <h4 class="mt-5 card-title text-center">Provas geradas em PDF.</h4>
-                        </div>
-                    </div>
-                </div>
+            <div class="card-body text-dark">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error repellat ullam fugit debitis nesciunt, eaque repudiandae molestiae dolorem blanditiis repellendus, distinctio aut. Eius facere reiciendis officiis. Hic, assumenda, sed. Iste!</p>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next"  href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only pink">Next</span>
-        </a>
     </div>
+    <div class="col col-lg-7 ml-3">
+        <div class="card ">
+            <div class="card-header">
+                <h5 class="card-title text-center">Provas geradas em PDF ou DOC.</h5>
+            </div>
+            <div class="card-body text-dark">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non expedita quaerat fuga rem inventore totam hic, aut facere ad. Quod nemo blanditiis ex tempore iure sapiente vel deserunt unde?</p>
+            </div>
+        </div>
+    </div>
+    <div class="col col-lg-5 mt-3 ml-5">
+        <div class="card ml-5">
+            <div class="card-header">
+                <h5 class="color text-center">Questões com nives de dificuldade.</h5>
+            </div>
+            <div class="card-body text-dark">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ullam, praesentium illo eum corrupti doloremque fugit saepe aspernatur nisi amet nulla, dolorem voluptate porro maiores labore quis sit repellendus laboriosam.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4 ml-3">
+    <div class="col col-lg-6 ml-5">
+        <div class="card ">
+            <div class="card-header">
+                <h5 class="card-title text-center">Crie ou adicione suas turmas</h5>
+            </div>
+            <div class="card-body text-dark">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non expedita quaerat fuga rem inventore totam hic, aut facere ad. Quod nemo blanditiis ex tempore iure sapiente vel deserunt unde?</p>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+
     @endsection
