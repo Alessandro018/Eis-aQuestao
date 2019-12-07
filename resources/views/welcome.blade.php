f27288aae3955ce578291c145a95f2a3f7bbac71
siilvatamires <tamiresmaria561@gmail.com>
2019-12-07 17:04:46 -0300

    GH-85 Correções na welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,12 @@
     @section('content')
 
 <img class="d-block w-100" src="{{ asset('img/layout.png') }}" alt="Card image">
-
-<h1 class="display-1 text-center mt-5">Eis-aQuestão</h1>
-
 <div class="container mt-5">
   <div class="row justify-content-md-center">
     <div class="col col-lg-4 mt-4">
         <div class="card">
             <div class="card-header">
-                <h5 class="color text-center">Coreção com QRcode.</h5>
+                <h5 class="color text-center">Correção com QRcode.</h5>
             </div>
             <div class="card-body text-dark">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error repellat ullam fugit debitis nesciunt, eaque repudiandae molestiae dolorem blanditiis repellendus, distinctio aut. Eius facere reiciendis officiis. Hic, assumenda, sed. Iste!</p>
@@ -21,7 +18,7 @@
     <div class="col col-lg-7 ml-3">
         <div class="card ">
             <div class="card-header">
-                <h5 class="card-title text-center">Provas geradas em PDF ou DOC.</h5>
+                <h5 class="card-title text-center">Provas geradas em PDF.</h5>
             </div>
             <div class="card-body text-dark">
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non expedita quaerat fuga rem inventore totam hic, aut facere ad. Quod nemo blanditiis ex tempore iure sapiente vel deserunt unde?</p>
@@ -31,24 +28,12 @@
     <div class="col col-lg-5 mt-3 ml-5">
         <div class="card ml-5">
             <div class="card-header">
-                <h5 class="color text-center">Questões com nives de dificuldade.</h5>
+                <h5 class="color text-center">Questões por níveis de dificuldades.</h5>
             </div>
             <div class="card-body text-dark">
                 <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ullam, praesentium illo eum corrupti doloremque fugit saepe aspernatur nisi amet nulla, dolorem voluptate porro maiores labore quis sit repellendus laboriosam.</p>
             </div>
         </div>
-    </div>
-    <div class="row mt-4 ml-3">
-    <div class="col col-lg-6 ml-5">
-        <div class="card ">
-            <div class="card-header">
-                <h5 class="card-title text-center">Crie ou adicione suas turmas</h5>
-            </div>
-            <div class="card-body text-dark">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non expedita quaerat fuga rem inventore totam hic, aut facere ad. Quod nemo blanditiis ex tempore iure sapiente vel deserunt unde?</p>
-            </div>
-        </div>
-    </div>
     </div>
 </div>
 
