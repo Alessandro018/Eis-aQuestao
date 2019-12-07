2b8ee03ced5e4c5205b3db3e0a2932f3daac2d93
robson-gomes <robson.gs2630@gmail.com>
2019-12-07 16:52:39 -0300

    GH-97 Ajuste no modal de quetões

--- resources/views/questoes/index.blade.php
+++ resources/views/questoes/index.blade.php
@@ -66,8 +66,8 @@
 			<div class="modal-footer border-0">
 				<input type="hidden" name="situacao" value="1">
 				<input type="hidden" name="professor_id" value="{{ Auth::user()->id }}">
-				<button type="submit" class="btn btn-success">Salvar</button>
-				<button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Fechar</button>
+				<button type="submit" class="btn btn-success">Criar</button>
+				<button type="button" class="btn btn-outline-danger" data-dismiss="modal">Cancelar</button>
 			</div>
 		</form>
 		</div>
