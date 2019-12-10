addfce99db7b8118a679b025af7ff082a54c0ab9
GuilhermeH6 <guilhermeholanda870@gmail.com>
2019-12-09 21:56:23 -0300

    GH-97 Alterando as cores dos botões da página de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,12 +53,12 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-orange">
                                     {{ __('Entrar') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link-secondary text-orange" href="{{ route('password.request') }}">
                                         {{ __('Esqueceu sua senha?') }}
                                     </a>
                                 @endif
