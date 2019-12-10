7e0faa54f14645780abf291a8616ec95adfca3cd
siilvatamires <tamiresmaria561@gmail.com>
2019-12-10 12:32:33 -0300

    GH-85 Correção dos textos da welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,48 +8,42 @@
     <p>O Eis a Questão é um sistema que possibilita a criação e correção automática de provas objetivas.</p>
 </div>
 <div class="container mt-5">
-  <div class="row justify-content-md-center">
-    <div class="col col-lg-4 mt-4">
-        <div class="card">
-            <div class="card-header">
-                <h5 class="color text-center text-dark">Correção com QRcode.</h5>
+    <div class="row justify-content-md-center">
+        <div class="card-deck">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title text-center text-dark">Correção com QRcode</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Correção automática de provas e gabaritos. Garante maior flexibilidade e praticidade para o professor na hora de corrigir as provas.
+                    </p>
+                </div>
             </div>
-            <div class="card-body">
-                <p>Correção automática de provas e gabaritos. Garante maior flexibilidade e praticidade para o professor na hora de corrigir as provas. </p>
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title text-center text-dark">Provas geradas em PDF</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Desta maneira, o professor pode criar provas individuais com questões embaralhadas prontas para impressão.</p>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col col-lg-7 ml-3">
-        <div class="card ">
-            <div class="card-header">
-                <h5 class="card-title text-center text-dark">Provas geradas em PDF.</h5>
+            <div class="card">
+                <div class="card-header pt-2 pb-1">
+                    <h5 class="mb-1 card-title text-center text-dark">Questões por níveis de dificuldade</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Através desta ferramenta, o professor pode criar provas contendo questões com vários níveis de dificuldade.</p>
+                </div>
             </div>
-            <div class="card-body">
-                <p>Desta maneira, o professor pode criar provas individuais com questões embaralhadas prontas para impressão.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col col-lg-5 mt-3 ml-5">
-        <div class="card ml-5">
-            <div class="card-header">
-                <h5 class="color text-center text-dark">Questões por níveis de dificuldade.</h5>
-            </div>
-            <div class="card-body">
-                <p>Através desta ferramenta, o professor pode criar provas contendo questões com vários níveis de dificuldade.</p>
-            </div>
-        </div>
-    </div>
-    <div class="row mt-4 ml-3">
-    <div class="col col-lg-6 ml-5">
-        <div class="card ">
-            <div class="card-header">
-                <h5 class="card-title text-center text-dark">Banco de Questões</h5>
-            </div>
-            <div class="card-body">
-                <p>Permite que professores criem questões que ficarão armazenas e poderão ser facilmente utilizadas para criação de novas provas.</p>
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title text-center text-dark">Banco de Questões</h5>
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Permite que professores criem questões que ficarão armazenadas e poderão ser facilmente utilizadas para criação de novas provas.</p>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </div>
 
