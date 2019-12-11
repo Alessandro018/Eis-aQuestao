94998f2245d6278cd5d868474c3f151eeebfa82f
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-12-11 14:44:45 -0300

    GH-105 pagina de sobre nos

---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+            
+@section('content')
+
+<h1 class="display-5 mt-5 text-center">Sobre Nós</h1>
+
+<div class="container mt-5">
+    <div class="row justify-content-md-center">
+        <div class="card-deck">
+            <div class="card">
+                <div class="card-body text-center ">
+					<h5 class="card-title">Robson</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Alessandro</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Gabriel</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Tamires</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Geovane</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row justify-content-md-center mt-4">
+        <div class="card-deck">
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Guilherme</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Filipe</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Alisson</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+					<h5 class="card-title">Ricardo</h5>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Pérola</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
