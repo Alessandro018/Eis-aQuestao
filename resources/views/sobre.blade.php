e8ea22ef1391695dbf1b8c8ab42531d243274a5f
AlissonThyago <alissonthiagoamorim@gmail.com>
2019-12-11 14:53:56 -0300

    GH-105 informações sobre os desenvolvedores

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -8,28 +8,48 @@
     <div class="row justify-content-md-center">
         <div class="card-deck">
             <div class="card">
+                <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/48128957?v=4">
                 <div class="card-body text-center ">
-					<h5 class="card-title">Robson</h5>
+                    <h5 class="card-title">Robson</h5>
+                    <a href="https://github.com/robson-gomes" class="card-link text-dark"><i class="fa fa-github large"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:robson.gs2630@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars1.githubusercontent.com/u/42873829?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Alessandro</h5>
+                    <h5 class="card-title">Alessandro</h5>
+                    <a href="https://github.com/Alessandro018" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:#" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars2.githubusercontent.com/u/10752327?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Gabriel</h5>
+                    <h5 class="card-title">Gabriel</h5>
+                    <a href="https://github.com/gabrielpessoa" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:#" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars0.githubusercontent.com/u/48129117?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Tamires</h5>
+                    <h5 class="card-title">Tamires</h5>
+                    <a href="https://github.com/siilvatamires" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:tamiresmariia27@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars0.githubusercontent.com/u/37213349?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Geovane</h5>
+                    <h5 class="card-title">Geovane</h5>
+                    <a href="https://github.com/geovanejose" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:#" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
         </div>
@@ -37,28 +57,48 @@
     <div class="row justify-content-md-center mt-4">
         <div class="card-deck">
             <div class="card">
+                <img class="card-img-top" src="https://avatars1.githubusercontent.com/u/47796068?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Guilherme</h5>
+                    <h5 class="card-title">Guilherme</h5>
+                    <a href="https://github.com/GuilhermeH6" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:guilhermeholanda870@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars3.githubusercontent.com/u/48837834?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Filipe</h5>
+                    <h5 class="card-title">Filipe</h5>
+                    <a href="https://github.com/filipeandrev" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:filipe.andrev7@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars2.githubusercontent.com/u/48837955?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Alisson</h5>
+                    <h5 class="card-title">Alisson</h5>
+                    <a href="https://github.com/AlissonThyago" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:mailto:alissonthiagoamorim@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars0.githubusercontent.com/u/42875766?v=4">
                 <div class="card-body text-center">
-					<h5 class="card-title">Ricardo</h5>
+                    <h5 class="card-title">Ricardo</h5>
+                    <a href="https://github.com/ricardomlp" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:#" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
             <div class="card">
+                <img class="card-img-top" src="https://avatars0.githubusercontent.com/u/49208567?v=4">
                 <div class="card-body text-center">
                     <h5 class="card-title">Pérola</h5>
+                    <a href="https://github.com/perolasanja" class="card-link text-dark"><i class="fa fa-github"></i></a>
+                    <a href="#" class="card-link text-dark"><i class="fa fa-linkedin"></i></a>
+                    <a href="mailto:peroladiniz2013@gmail.com" class="card-link text-dark"><i class="fa fa-envelope"></i></a>
                 </div>
             </div>
         </div>
