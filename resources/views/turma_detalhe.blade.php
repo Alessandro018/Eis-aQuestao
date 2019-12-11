c2fe1042df91a98fe87c07eb7cb21575aa1b44a8
filipeandrev <filipe.andrev7@gmail.com>
2019-12-11 13:55:24 -0300

    GH-92 Alterando cor do header e da fonte dos titulos

--- resources/views/turma_detalhe.blade.php
+++ resources/views/turma_detalhe.blade.php
@@ -3,13 +3,15 @@
     <h2>Detalhe da turma</h2>
     <div class="form-group">
         <table class="table table-sm mt-3">
-            <tr>
-                <th>Professores</th>
-                <th>Curso</th>
-                <th>Disciplina</th>
-                <th>Periodo letivo</th>
-                <th>turno</th>
-            </tr>
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Professores</th>
+                    <th scope="col">Curso</th>
+                    <th scope="col">Disciplina</th>
+                    <th scope="col">Periodo letivo</th>
+                    <th scope="col">turno</th>
+                </tr>
+            </thead>
                 <tr>
                 <td>
                 @foreach($professores as $professor)
