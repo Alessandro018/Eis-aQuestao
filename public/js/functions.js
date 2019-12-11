e302ccf26c9b590032a2e5846c2940bfd289156a
siilvatamires <tamiresmaria561@gmail.com>
2019-12-11 12:02:52 -0300

    GH-104 Fazendo algumas alterações no arquivo functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -15,18 +15,18 @@ $(document).ready(function(){
         })
     })
 
-    $('form#prova, select[name="curso"]').on('change', function(){
+    $('form#prova select[name="curso"]').on('change', function(){
         var curso = $(this).val();
-        var professor = $('form#prova, input[name="professor"]').val();
+        var professor = $('form#prova input[name="professor"]').val();
         $.ajax({
             url: '/api/turmas',
             type: 'POST',
             data: { curso: curso, professor: professor },
             success: function (retorno){
-                $('form#prova, select[name="turma_id"]').empty();
-                $('form#prova, select[name="turma_id"]').removeAttr("disabled");
+                $('form#prova select[name="turma_id"]').empty();
+                $('form#prova select[name="turma_id"]').removeAttr("disabled");
                 for(let i in retorno){
-                    $('form#prova, select[name="turma_id"]').append("<option value="+retorno[i].id+">"+retorno[i].nome
+                    $('form#prova select[name="turma_id"]').append("<option value="+retorno[i].id+">"+retorno[i].nome
                     +" - "+retorno[i].ano+"."+retorno[i].semestre+" | "+retorno[i].turno+"</option>");
                 $('form#prova').append('<input type="hidden" name="disciplina_id" value="'+ retorno[i].disciplina+'">');
                 }
@@ -39,31 +39,39 @@ $(document).ready(function(){
         }
     })
 
-    $('button#editar').on('click', function(e){
-        e.preventDefault();
-        const value = JSON.parse($(this).attr('value'));
-        var target = $(this).attr('data-target');
-        $('body').toggleClass('modal-open');
-        $('div#confirm').toggleClass('show');
-        $('body').append('<div class="modal-backdrop fade show"></div>');
-        $('div#confirm').css('display', 'block');
-        $('div#confirm').attr('aria-modal', 'true');
-        $('div#confirm').attr('id', target);
-        $('form#prova, select[name="turma_id"]').html('<option>'+value.nome+' - '+value.ano + value.semestre + ' | ' + value.turno);
-        $('form#prova, select[name="curso"], option[value="'+value.curso_id+'"]').attr('selected', "");
-        $('form#prova, button[type="submit"]').html('Atualizar');
-        $('form#prova, select[name="turma_id"]').removeAttr("disabled");
-
-        // $.ajax({
-        //     url: '',
-        //     type: 'POST',
-        //     data: {id_curso: value.curso_id},
-        //     success: function(retorno){
-
-        //     }
-        // })
-        // console.log(value);
+    $('.criar_prova, .editar_prova').on('click', function(e) {
+        const modalId = $(this).data('target');
+        const action = $(this).data('action');
+        const actionText = $(this).data('action-text');
+        $(`${modalId} .action_btn`).html(actionText);
+        $(`${modalId} form`).attr('action', action);
     })
+
+    // $('button#editar').on('click', function(e){
+    //     e.preventDefault();
+    //     const value = JSON.parse($(this).attr('value'));
+    //     var target = $(this).attr('data-target');
+    //     $('body').toggleClass('modal-open');
+    //     $('div#confirm').toggleClass('show');
+    //     $('body').append('<div class="modal-backdrop fade show"></div>');
+    //     $('div#confirm').css('display', 'block');
+    //     $('div#confirm').attr('aria-modal', 'true');
+    //     $('div#confirm').attr('id', target);
+    //     $('form#prova, select[name="turma_id"]').html('<option>'+value.nome+' - '+value.ano + value.semestre + ' | ' + value.turno);
+    //     $('form#prova, select[name="curso"], option[value="'+value.curso_id+'"]').attr('selected', "");
+    //     $('form#prova, button[type="submit"]').html('Atualizar');
+    //     $('form#prova, select[name="turma_id"]').removeAttr("disabled");
+
+    //     // $.ajax({
+    //     //     url: '',
+    //     //     type: 'POST',
+    //     //     data: {id_curso: value.curso_id},
+    //     //     success: function(retorno){
+
+    //     //     }
+    //     // })
+    //     // console.log(value);
+    // })
 
     $('div#curso select[name="curso"]').on('change', function(){
         var curso = $(this).val();
