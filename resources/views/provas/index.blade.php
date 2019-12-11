95d73700192666b25441a5fc0e87d0411263bd9f
siilvatamires <tamiresmaria561@gmail.com>
2019-12-10 22:40:30 -0300

    GH-97 Alterando classe do botão de editar prova

--- resources/views/provas/index.blade.php
+++ resources/views/provas/index.blade.php
@@ -150,8 +150,8 @@
                 <form action="{{ action('ProvaController@destroy',$prova->id) }}" method="POST">
 					@csrf
                     @method('DELETE')
-                        <button type="button" id="editar" class="btn btn-outline-secondary" value="{{$prova}}"data-toggle="modal" data-target="#modal_{{ $prova->id }}">Editar</button>
-                        <button type="button" class="btn btn-outline-dark" data-toggle="modal" data-target="#confirm_{{ $prova->id }}">Excluir</button>
+                        <button type="button" id="editar" class="btn btn-secondary" value="{{$prova}}"data-toggle="modal" data-target="#modal_{{ $prova->id }}">Editar</button>
+                        <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#confirm_{{ $prova->id }}">Excluir</button>
 
                         <div class="modal fade" id="confirm_{{ $prova->id }}" role="dialog">
                             <div class="modal-dialog modal-md">
